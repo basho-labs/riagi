37a80a84bd5f52d59166c04d38dd79a1f46deaa0
Mathias Meyer <meyer@paperplanes.de>
2011-07-04 23:10:01 +0200

    Fix indentation

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 import os
-      os.environ['DJANGO_SETTINGS_MODULE'] = '.'
 
-      import django.core.handlers.wsgi
-      application = django.core.handlers.wsgi.WSGIHandler()
+os.environ['DJANGO_SETTINGS_MODULE'] = '.'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()

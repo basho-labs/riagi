8440d195a199faa24d19a26643b9c7167df2a928
Mathias Meyer <meyer@paperplanes.de>
2011-07-08 18:41:34 +0200

    Improve signup layout.

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from django.forms.util import ErrorList
+
+class DivErrorList(ErrorList):
+     def __unicode__(self):
+         return self.as_divs()
+     def as_divs(self):
+         if not self: return u''
+         return u'<div class="errorlist">%s</div>' % ''.join([u'<div class="error">%s</div>' % e for e in self])
 
 class SignupForm(forms.Form):
-    email = forms.EmailField()
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'text'}))
+    username = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'text'}))
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'class':'text'}))
 
 class LoginForm(forms.Form):
     pass
--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from users.forms import SignupForm
+from django.template import RequestContext
+from users.forms import DivErrorList
 
 def signup(request):
+    signup_form = SignupForm(request.POST, label_suffix="<br/>", error_class=DivErrorList)
     if request.method == "POST":
-        signup_form = SignupForm(request.POST)
         if signup_form.is_valid():
             signup_form.save()
     else:
-        signup_form = SignupForm()
+        pass
     
-    return render_to_response('users/signup.html', {'signup_form': signup_form})
+    return render_to_response('users/signup.html', {'signup_form': signup_form},
+            context_instance=RequestContext(request))

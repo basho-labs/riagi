ef0e927800f03af3fd417a0c02981bd4d08de12e
Klynton <klynton@objectsyndicate.com>
2012-11-13 10:10:10 -0700

    Updating to Django 1.4.2's new project structure and fixing imports for new structure.

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('users.views',
-    url(r'^signup$', 'signup'),
-    url(r'^logout$', 'logout'),
-    url(r'^login$', 'login'),
-)
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
